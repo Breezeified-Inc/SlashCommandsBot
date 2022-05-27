d45bb7a72327e4602317f2b4da69688636bb4784
Scripted <62614570+Scriptedisles@users.noreply.github.com>
2022-05-27 09:28:22 -0500

    Add crack

--- bot.js
+++ bot.js
@@ -1,1 +1,64 @@
+const Discord = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+
+const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
+const prefix = "!";
+
+const fetch = require('node-fetch');
+const { Messages, Guild } = require('discord.js');
+
+
+
+
+
+const client = new Client({
+  // Get Intents
+  intents: new Intents([
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES
+  ])
+  
+})
+
+
+
+
+client.commands = new Discord.Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN)
 
